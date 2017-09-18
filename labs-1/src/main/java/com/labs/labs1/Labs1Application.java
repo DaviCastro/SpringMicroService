9a848fadb78ffd5fe89fa12208f8b725d28cb6e0
lopescad <davidson.lopescastro@br.unisys.com>
2017-09-18 19:26:46 -0300

    yml ConfigData

---
+++ labs-1/src/main/java/com/labs/labs1/Labs1Application.java
@@ -0,0 +1,12 @@
+package com.labs.labs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labs1Application.class, args);
+	}
+}
