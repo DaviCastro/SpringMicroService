c6978b6231294277e36bcbb3640ad2c3e4f57eb5
lopescad <davidson.lopescastro@br.unisys.com>
2017-09-20 19:38:49 -0300

    lab4 quase completo

---
+++ lab-4-verb/src/main/java/com/example/labs/controllers/WordController.java
@@ -0,0 +1,29 @@
+package com.example.labs.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author Davidson
+ *
+ */
+
+@RestController
+public class WordController {
+
+	@Value("${words}")
+	private String words;
+
+	@GetMapping("/")
+	public @ResponseBody String getWord() {
+
+		String[] wordArray = words.split(",");
+		int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+
+	}
+
+}
