c84f9ad0a39e24a0833742940f76a250cbe4f4af
lopescad <davidson.lopescastro@br.unisys.com>
2017-11-13 21:01:12 -0200

    lab-7

---
+++ lab-7-word-server/run-all-without-adjective.sh
@@ -0,0 +1,4 @@
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-subject" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-verb" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-article" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-noun" 
---
+++ lab-7-word-server/run-all.sh
@@ -0,0 +1,5 @@
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-subject" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-verb" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-article" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-adjective" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=lab-4-noun" 
